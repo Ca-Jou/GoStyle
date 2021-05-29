6c934bd727cf2628c051eefb49e5a6bdd6e4b3d3
Ca-Jou <camille.pauron@gmail.com>
2021-05-29 22:39:04 +0200

    fix CouponsTest

--- tests/API/CouponsTest.php
+++ tests/API/CouponsTest.php
@@ -30,7 +30,7 @@ class CouponsTest extends WebTestCase
 
         // retrieve token
         $userRepository = static::$container->get(UserRepository::class);
-        $testUser = $userRepository->findOneBy(['username' => 'Camille']);
+        $testUser = $userRepository->findOneBy(['username' => 'TestUser1']);
 
         // token provided -> it should fail with a 404 (collection retrieval deactivated in the API)
         $client->request('GET', '/api/coupons', [], [], [
@@ -45,7 +45,7 @@ class CouponsTest extends WebTestCase
         $client = static::createClient();
 
         // No authentication -> it should ask for credentials
-        $client->request('GET', '/api/coupons/20CASQ');
+        $client->request('GET', '/api/coupons/TEST1');
         $this->assertResponseStatusCodeSame(401);
         $this->assertResponseHeaderSame('content-type', 'application/json');
 
@@ -61,10 +61,10 @@ class CouponsTest extends WebTestCase
 
         // retrieve token
         $userRepository = static::$container->get(UserRepository::class);
-        $testUser = $userRepository->findOneBy(['username' => 'Camille']);
+        $testUser = $userRepository->findOneBy(['username' => 'TestUser1']);
 
         // token provided -> it should succeed
-        $client->request('GET', '/api/coupons/20CASQ', [], [], [
+        $client->request('GET', '/api/coupons/TEST1', [], [], [
             'HTTP_X_AUTH_TOKEN' => $testUser->getApiToken()
         ]);
         $this->assertResponseIsSuccessful();
@@ -72,7 +72,7 @@ class CouponsTest extends WebTestCase
 
         $responseContent = json_decode($client->getResponse()->getContent(), true);
         $this->assertArrayHasKey('code', $responseContent);
-        $this->assertSame('20CASQ', $responseContent['code']);
+        $this->assertSame('TEST1', $responseContent['code']);
         $this->assertArrayHasKey('description', $responseContent);
         $this->assertArrayHasKey('begins', $responseContent);
         $this->assertArrayHasKey('ends', $responseContent);
@@ -86,7 +86,7 @@ class CouponsTest extends WebTestCase
 
         // retrieve token
         $userRepository = static::$container->get(UserRepository::class);
-        $testUser = $userRepository->findOneBy(['username' => 'Camille']);
+        $testUser = $userRepository->findOneBy(['username' => 'TestUser1']);
 
         $testCoupon = new Coupon();
         $testCoupon->setCode('TEST');
