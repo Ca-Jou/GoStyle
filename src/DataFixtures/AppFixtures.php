bfed8b192aaf871f1dd9b40ba2dee8ae5b730700
Ca-Jou <camille.pauron@gmail.com>
2021-06-02 18:21:31 +0200

    update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -24,14 +24,14 @@ class AppFixtures extends Fixture implements FixtureGroupInterface
     public function load(ObjectManager $manager)
     {
         $coupon1 = new Coupon();
-        $coupon1->setCode("20CASQ");
+        $coupon1->setCode("GS20CASQ");
         $coupon1->setDescription("-20% sur les casquettes");
         $coupon1->setBegins(new \DateTime("20210415"));
         $coupon1->setEnds(new \DateTime("20210502"));
         $manager->persist($coupon1);
 
         $coupon2 = new Coupon();
-        $coupon2->setCode("DECONFINE");
+        $coupon2->setCode("GSDECONFI");
         $coupon2->setDescription("-50% sur tout le site");
         $coupon2->setBegins(new \DateTime("20210502"));
         $coupon2->setEnds(new \DateTime("20210502"));
